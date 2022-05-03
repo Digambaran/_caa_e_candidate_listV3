862d466467d2a216f989c703b3db145a2e58c157
Jishnu <jishnu.nb@neoito.com>
2022-05-03 15:15:40 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import e_candidate_list from './e_candidate_list'
+export default function App() {
+  return (
+    <div>
+      <e_candidate_list/>
+    </div>
+  )
+}
---
+++ src/e_candidate_list.js
@@ -0,0 +1,8 @@
+import React from 'react'
+// import env from 'env'
+export const e_candidate_list = () => {
+  return (
+   <p>Hello from e_candidate_list </p>
+  )
+}
+export default e_candidate_list
